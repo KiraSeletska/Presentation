ce1323e7767ba2ccc6a46d5b785ce82a24e8e1bf
KiraSeletska <seletskaya.kira@gmail.com>
2023-10-18 13:49:28 +0200

    fix: footer & in mobV

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,5 @@
 import styles from "./footer.module.scss";
-import jsImg from "../../images/icons_coding/logo-javascript.svg";
-import tsImg from "../../images/icons_coding/typescript.svg";
-import reactImg from "../../images/icons_coding/react-2.svg";
-import reduxImg from "../../images/icons_coding/redux.svg";
-import sassImg from "../../images/icons_coding/sass-1.svg";
-import mobXImg from "../../images/icons_coding/mobx.svg";
+
 import emailLogo from "../../images/icons/gmail-icon-2.svg"
 import githubLogo from "../../images/icons/github-icon-1.svg"
 import linkedinLogo from "../../images/icons/linkedin-icon-2.svg"
@@ -16,15 +11,6 @@ export const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
       <div className={styles.wrapper}>
-        <div className={styles.icons}>
-          <img src={jsImg} alt="Javascript" />
-          <img src={tsImg} alt="Typescript" />
-          <img src={reactImg} alt="React" />
-          <img src={reduxImg} alt="Redux" />
-          <img src={mobXImg} alt="MobX" />
-          <img src={sassImg} alt="Sass" />
-        </div>
-        <div className={styles.center}><p></p></div>
         <div className={styles.contacts}>
           <ul>
             <li>
